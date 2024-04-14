983d191fa41484807127843982158b45682149bf
Loraan <bfire2066@gmail.com>
2024-04-14 22:50:31 +0500

    Add first test

---
+++ SeleniumTests_Golovachev/SeleniumTestsForPractice.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace SeleniumTests_Golovachev;
+
+public class SeleniumTestsForPractice
+{
+    [Test]
+    public void Authorization()
+    {
+        var option = new ChromeOptions();
+        option.AddArguments("--no-sandbox", "--start-maximized", "--disable-extensions");
+        
+        var driver = new ChromeDriver(option);
+        var url = "https://staff-testing.testkontur.ru";
+        driver.Navigate().GoToUrl(url);
+        Thread.Sleep(5000);
+        
+        var login = driver.FindElement(By.Id("Username"));
+        login.SendKeys("bfire2066@gmail.com");
+        var password = driver.FindElement(By.Id("Password"));
+        password.SendKeys("Bl0@dFire63");
+        Thread.Sleep(5000);
+        
+        var enter = driver.FindElement(By.Name("button"));
+        enter.Click();
+        Thread.Sleep(5000);
+
+        var currentUrl = driver.Url;
+        Assert.That(currentUrl == "https://staff-testing.testkontur.ru/news");
+        
+        driver.Quit();
+    }
+}
